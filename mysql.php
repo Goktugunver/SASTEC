d1773b666a94ceb283894b8e63a40264e70d14ad
Goktug Unver <goktuunver@gmail.com>
2020-08-28 17:19:26 +0300

    Revert "Create mysql.php"
    This reverts commit b5cad1bb0f4b957caeb1f5f2e5821109b1e769cf.

--- mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$MyUsername = “kullaniciadiniz”;  // 
-$MyPassword = “parolaniz”;  // 
-$MyHostname = “localhost”;      // 
-$dbh = mysql_pconnect($MyHostname , $MyUsername, $MyPassword);
-$selected = mysql_select_db(“test”,$dbh);
-?>
